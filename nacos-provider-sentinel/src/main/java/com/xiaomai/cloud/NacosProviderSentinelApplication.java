161723b0a0d8d8ad605090bebcc15d135215d836
wangfeng <123456789@qq.com>
2020-11-04 22:15:38 +0800

    no message

--- nacos-provider-sentinel/src/main/java/com/xiaomai/cloud/NacosProviderSentinelApplication.java
+++ nacos-provider-sentinel/src/main/java/com/xiaomai/cloud/NacosProviderSentinelApplication.java
@@ -1,8 +1,23 @@
 package com.xiaomai.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 /**
  * @author wangfeng
  * @date 2020/11/4
  */
+@SpringBootApplication
 public class NacosProviderSentinelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderSentinelApplication.class,args);
+    }
+
+    @PostConstruct
+    void setDefaultTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+    }
 }
